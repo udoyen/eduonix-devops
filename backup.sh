67a57fe0cde528c4052c8d95d550a209f719f32a
udoyen <datameshprojects@gmail.com>
2021-10-11 14:26:56 +0100

    rsync code done

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-#!/usr/bash
+#!/usr/bin/env bash
 
 # Script to compress and backup files using resync
 
@@ -8,22 +8,25 @@ readonly SOURCE_DIR="${HOME}"
 readonly BACKUP_DIR="/data/backups"
 readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
 readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
-readonly LATES_LINK="${BACKUP_DIR}/latest"
+readonly LATEST_LINK="${BACKUP_DIR}/latest"
 
 # Create the backup directory
 
 mkdir -p "${BACKUP_DIR}"
 
-# Compress the home directory
-
-
-
 # Run the rsync command to start the backup process
 
 rsync -av --delete --copy-links --compress "${SOURCE_DIR}/" --link-dest "${LATEST_LINK}" --exclude=".cache" "${BACKUP_PATH}"
 
-
 # Remove the previous backup
 
 rm -rf "${LATEST_LINK}"
-ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
+ln -s "${LATEST_LINK}" "${LATEST_LINK}"
+
+# compress files in backup directory
+
+tar -cJf "${LATEST_LINK}"/home.tar.xz  "${LATEST_LINK}"/*
+
+# Delete duplicate files
+
+find "${LATEST_LINK}"/*  -not -name "home*" -not -name "." -not -name ".." -exec rm -rf {} \;
